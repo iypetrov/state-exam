c9faa79f4e0413cec6687f1118fd3351a73bae01
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-08-29 12:24:34 +0300

    feat: queue
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ queue/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* back;
+
+public:
+    Queue() : front(nullptr), back(nullptr) {}
+    ~Queue() {
+        Node* curr = front;
+        while(curr) {
+            cout << curr->data << endl;
+            curr = curr->next;
+        }
+    }
+
+    bool empty() {
+        if(front == nullptr) {
+            return true;
+        }
+        return false;
+    }
+
+    void print() {
+        Node* curr = front;
+        while(curr) {
+            cout << curr->data << endl;
+            curr = curr->next;
+        }
+    }
+
+    void push(int x) {
+        Node* curr = new Node(x);
+        if (front == nullptr) {
+            front = curr;
+            back = curr;
+            return;
+        }
+
+        back->next = curr;
+        back = curr;
+    }
+
+    void pop() {
+        if (front == nullptr) {
+            return;
+        }
+
+        Node* curr = front;
+        front = front->next;
+        if (front == nullptr) {
+            back = nullptr;
+        }
+        delete curr;
+    }
+
+    int peek() {
+        if (front == nullptr) {
+            return -1;
+        }
+        return front->data;
+    }
+};
+
+int main() {
+    return 0;
+}
